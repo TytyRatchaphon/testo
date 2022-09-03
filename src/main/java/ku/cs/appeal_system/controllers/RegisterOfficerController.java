a391faf12a7c2e87f08bfc6dc721972d8b891bbe
thanwaralee <thanwaralee.t@ku.th>
2022-09-03 20:52:10 +0700

    Thanwaralee

--- src/main/java/ku/cs/appeal_system/controllers/RegisterOfficerController.java
+++ src/main/java/ku/cs/appeal_system/controllers/RegisterOfficerController.java
@@ -7,18 +7,19 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import ku.cs.appeal_system.models.Account;
 import ku.cs.appeal_system.models.AccountList;
+import ku.cs.appeal_system.models.OfficerAccount;
 import ku.cs.appeal_system.services.AccountFileDataSource;
 import ku.cs.appeal_system.services.DataSource;
 
 import java.io.IOException;
 
 public class RegisterOfficerController {
-    @FXML private TextField nameTextField;
-    @FXML private TextField lastnameTextField;
+    @FXML private TextField newNameTextField;
+    @FXML private TextField newLastNameTextField;
     @FXML private TextField agencyTextField;
-    @FXML private TextField userNameTextField;
-    @FXML private PasswordField password;
-    @FXML private PasswordField confirmPassword;
+    @FXML private TextField newUserNameTextField;
+    @FXML private PasswordField newPasswordPasswordField;
+    @FXML private PasswordField confirmPasswordField;
 
     @FXML
     public void handleSubmitButton(ActionEvent event) {
@@ -27,17 +28,17 @@ public class RegisterOfficerController {
         AccountList accounts = dataSource.readData();
 
         // รับข้อมูล จาก nameTextField ข้อมูลที่รับเป็น String เสมอ
-        String nameStr = nameTextField.getText(); // ตัวแปร name
+        String nameStr = newNameTextField.getText(); // ตัวแปร name
         // รับข้อมูล จาก lastNameTextField ข้อมูลที่รับเป็น String เสมอ
-        String lastnameStr = lastnameTextField.getText(); // ตัวแปร lastName
+        String lastnameStr = newLastNameTextField.getText(); // ตัวแปร lastName
         // รับข้อมูล จาก userNameTextField ข้อมูลที่รับเป็น String เสมอ
         String agency = agencyTextField.getText(); // ตัวแปร agency
         //รรับข้อมูล จาก agencyTextField ข้อมูลที่รับเป็น String เสมอ
-        String usernameStr = userNameTextField.getText(); // ตัวแปร userName
+        String usernameStr = newUserNameTextField.getText(); // ตัวแปร userName
         // รับข้อมูล จาก PasswordField ข้อมูลที่รับเป็น String เสมอ
-        String passwordStr = password.getText(); // ตัวแปร password
+        String passwordStr = newPasswordPasswordField.getText(); // ตัวแปร password
         // รับข้อมูล จาก PasswordField ข้อมูลที่รับเป็น String เสมอ
-        String confirmPasswordStr = confirmPassword.getText(); // ตัวแปร confirmPassword
+        String confirmPasswordStr = confirmPasswordField.getText(); // ตัวแปร confirmPassword
 
 
 
@@ -63,13 +64,13 @@ public class RegisterOfficerController {
                     Alert alert = new Alert(Alert.AlertType.INFORMATION);
                     alert.setTitle("Error!!");
                     alert.setHeaderText(null);
-                    alert.setContentText("Please check your information and try again.");
+                    alert.setContentText("Password is not match.");
 
                     alert.showAndWait();
 
                 } else {
                     //เขียนไฟล์ --> new Account
-                    accounts.addAccount(new Account(nameStr,lastnameStr ,usernameStr ,passwordStr ));
+                    accounts.addAccount(new OfficerAccount(usernameStr,passwordStr,nameStr,lastnameStr,agency));
                     dataSource.writeData(accounts);
 
                     try {
@@ -85,7 +86,7 @@ public class RegisterOfficerController {
     }
     public void handleBackButton(ActionEvent action){
         try {
-            com.github.saacsos.FXRouter.goTo("login");
+            com.github.saacsos.FXRouter.goTo("home_admin");
         } catch (IOException e) {
             System.err.println("ไปที่หน้า login ไม่ได้");
             System.err.println("ให้ตรวจสอบการกำหนด route");
